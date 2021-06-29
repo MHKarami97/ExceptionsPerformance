58ef08fe6eb7ee1b77920ab9fb1bab5354b2145c
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-06-29 14:20:08 +0430

    init project

---
+++ ExceptionsPerformance/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ExceptionsPerformance
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await Task.Run(WithException);
+            var result1 = await Task.Run(WithOutException);
+
+            Console.WriteLine($"WithException: {result} , TotalMilliSecond: {result.Milliseconds}");
+            Console.WriteLine($"WithOutException: {result1} , TotalMilliSecond: {result1.Milliseconds}");
+        }
+
+        static TimeSpan WithException()
+        {
+            var watch = new Stopwatch();
+            var list = new List<string>();
+
+            for (var i = 0; i < Math.Pow(10, 6); i++)
+            {
+                if (i == 1 || i % 2 != 0)
+                {
+                    list.Add(i.ToString());
+                }
+                else
+                {
+                    list.Add(null);
+                }
+            }
+
+            watch.Start();
+
+            foreach (var s in list)
+            {
+                try
+                {
+                    var i = s.Length;
+                }
+                catch (Exception ex)
+                {
+                    var e = ex.Message;
+                }
+            }
+
+            watch.Stop();
+
+            return watch.Elapsed;
+        }
+
+        static TimeSpan WithOutException()
+        {
+            var watch = new Stopwatch();
+            var list = new List<string>();
+
+            for (var i = 0; i < Math.Pow(10, 6); i++)
+            {
+                if (i == 1 || i % 2 != 0)
+                {
+                    list.Add(i.ToString());
+                }
+                else
+                {
+                    list.Add(null);
+                }
+            }
+
+            watch.Start();
+
+            foreach (var s in list)
+            {
+                if (s == null)
+                    continue;
+
+                var i = s.Length;
+            }
+
+            watch.Stop();
+
+            return watch.Elapsed;
+        }
+    }
+}
